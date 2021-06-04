b96690892926cba611b08ec8a0ba547513506a5a
Daniel Gray <dieseldan14@gmail.com>
2021-06-04 18:38:50 -0500

    Add project files.

---
+++ PhoneBookLab/PhoneBook/Contacts.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookLab
+{
+    public class Contacts
+    {
+        public string fName;
+        public string lName;
+        public string address;
+        public string phoneNumber;
+        
+
+        public List<Contacts> People = new List<Contacts>();
+
+
+        public void DisplayContacts()
+        {
+
+        }
+
+        public void addUser()
+        {
+            #region addUser
+            bool isCorrect = true;
+            while (isCorrect) // change to a foreach loop if possible
+            {
+
+                Contacts person = new Contacts();
+                Console.WriteLine("Please enter in this order, First name, Last name, address, and phone number: and be sure to press enter after each field: ");
+                person.fName = Console.ReadLine();
+                person.lName = Console.ReadLine();
+                person.address = Console.ReadLine();
+                person.phoneNumber = Console.ReadLine();
+                People.Add(person);
+
+                Console.WriteLine($"You added {person.fName} {person.lName}, address: {person.address}, phone number: {person.phoneNumber} " + " Is that correct? Y/N: ");
+                var correct = Console.ReadLine();
+                if (correct.ToLower() == "y")
+                {
+                    Console.WriteLine("Alright, we got them added for you!! ");
+                    isCorrect = false;
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Sorry bout that, lets try again! ");
+                    isCorrect = true;
+                    continue;
+                }
+
+
+            }
+            #endregion  
+        }
+        public void editUser()
+        {
+            Console.WriteLine("Edit User");
+        }
+        public void deleteUser()
+        {
+            Console.WriteLine("Delete User");
+        }
+        public void viewAllContacts()
+        {
+
+        }
+    }
+}
---
+++ PhoneBookLab/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookLab
+{
+    public class Program : Contacts
+    {
+        public static void Main(string[] args)
+        {
+            RunPhoneBook();
+        }
+
+        private static void RunPhoneBook()
+        {
+            Contacts pb1 = new Contacts();
+            bool cont1 = true;
+            while (cont1)
+            {
+
+
+                bool phonebookIsRunning = true;
+                while (phonebookIsRunning)
+                {
+                    Console.Write("Hello, Welcome to the PhoneBook!" + "\n" + "Please choose one of the following options" + "\n" + "1. Add someone to PhoneBook " + "\n" +
+                    "2. Remove Someone From PhoneBook " + "\n" + "3. View All PhoneBook Entries (Contacts) " + "\n" + "4. Edit Existing Contact" +
+                    "\n" + "5. Exit " + "\n" + "\n");
+                    var userEntry = Convert.ToInt32(Console.ReadLine());
+
+
+                    switch (userEntry)
+                    {
+                        case 1:
+                            pb1.addUser();
+                            phonebookIsRunning = false;
+                            break;
+                        case 2:
+                            pb1.deleteUser();
+                            phonebookIsRunning = false;
+                            break;
+                        case 3:
+                            pb1.viewAllContacts();
+                            phonebookIsRunning = false;
+                            break;
+                        case 4:
+                            pb1.editUser();
+                            phonebookIsRunning = false;
+                            break;
+                        case 5:
+                            Console.WriteLine("Thank you for using my Phonebook!!");
+                            phonebookIsRunning = false;
+                            break;
+                        default:
+                            Console.WriteLine("I do not recognize that selection, please try again.");
+                            phonebookIsRunning = true;
+                            break;
+
+                    }
+
+
+                }
+                Console.WriteLine("Would You like to continue? y/n: ");
+                var cont = Console.ReadLine();
+
+                if (cont.ToLower() == "y")
+                {
+                    cont1 = true;
+                    phonebookIsRunning = true;
+                }
+                else
+                {
+                    Console.WriteLine("Thank You for using this application!");
+                    cont1 = false;
+                    phonebookIsRunning = false;
+
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
